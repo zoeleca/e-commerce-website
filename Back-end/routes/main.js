ddc1238599790bfaf3a1cab63ab0471f8fbe82ce
PDeslousPaoli <philippe.deslous-paoli@laposte.net>
2024-03-06 20:59:14 +0100

    routes pipou

---
+++ Back-end/routes/main.js
@@ -0,0 +1,21 @@
+//main.js
+
+const express = require("express");
+const router = express.Router();
+const { getProduct } = require("./Back-end/dbQueries");
+
+router.get("/", function(req, res, next) {
+  console.log("Router Working");
+  res.sendFile("/test.html");
+});
+  
+router.get("/", async (req, res) => {
+  try {
+    const products = await getProduct();
+    res.json(products);
+  } catch (error) {
+    console.error("Error executing SQL query", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
