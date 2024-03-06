b4d1f82e5977b3c4560724b89e47af03820ea678
zoeleca <zoennts@gmail.com>
2024-03-06 07:46:36 +0100

    querySQL exemple comments

---
+++ Back-end/modules/dbConfig.js
@@ -0,0 +1,63 @@
+//Importation du module 'pg'
+const { Client } = require('pg');
+
+//Creation d'une instance du client PostgreSQL
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'pw',
+    port: 5432,
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 3000
+});
+
+
+//Connection à la DB
+const connectClient = async () =>{
+    try {
+        await client.connect();
+        console.log('Connected to database'); 
+        return client;
+    }catch (err) {
+        console.error('Could not connect to database:', err.stack);
+        throw err;
+    }
+}
+
+//Execution de requêtes SQL
+const executeQuery = async (querySQL) => {
+    try {
+        const connectedCLient = await connectClient();
+        const result = await connectedCLient.query(querySQL);
+        return result.rows;
+    }catch (err){
+        console.log('Error executing query:', err.stack);
+        throw err;
+    }finally{
+        await client.end();
+    }
+}
+
+// Exportation de la fonction 'executeQuery'
+module.exports = executeQuery;
+
+/*exemple de requete
+
+//importation function executeQuery 
+const executeQuery = require('./dbConfig);
+
+//Création requete SQL 
+const userQuery = 'SELECT * FROM public.user_account ORDER BY id ASC';
+
+//Utilisation de executeQuery function; 
+async function runUserQuery() {
+    try {
+        const userAccounts = await executeQuery(userQuery);
+        console.log('User accounts:', userAccounts);
+    } catch (error) {
+        console.error('Error fetching user accounts:', error.stack);
+    }
+}
+runUserQuery();
+*/
