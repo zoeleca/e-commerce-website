16c64588ff93cde16df60a58b23388fc3a2d881a
PDeslousPaoli <philippe.deslous-paoli@laposte.net>
2024-03-07 16:09:40 +0100

    Corrected server query

--- Back-end/routes/main.js
+++ Back-end/routes/main.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const { getProduct } = require("./Back-end/dbQueries");
+const { getProduct } = require("./Back-end/dbQueries.js");
 
 router.get("/", function(req, res, next) {
   console.log("Router Working");
