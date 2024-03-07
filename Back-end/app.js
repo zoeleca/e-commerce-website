deb5669b971e8ffd68952533c92a7437cf573891
MorganeLM <lets.morganise.it@gmail.com>
2024-03-07 16:23:03 +0100

    route detail

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,12 +1,22 @@
+//app.js
+
 // importe le module Express
-const express = require('express');
+const express = require("express");
 
 // crée une instance 'app' de Express
 const app = express();
 
+// Middleware pour parser le JSON dans le corps des requêtes
+app.use(express.json());
+
+/// Importe les routes de detail
+const detailRoutes = require("./routes/detail");
+// utilise les routes de detail sur le chemin "/detail"
+app.use("/detail", detailRoutes);
+
 // exemple de fonction middleware renvoyant un json
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien bien été reçue !' }); 
+res.json({ message: "Votre requête a bien été reçue !" });
 });
 
 // on exporte l'application Express (encapsulée)
