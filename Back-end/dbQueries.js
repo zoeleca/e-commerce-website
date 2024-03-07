55a89f651543018bfd7486c73875bd0184c55a20
Marion Ochem <marion.ochem@gmail.com>
2024-03-07 16:14:36 +0100

    minor debuging modification

--- Back-end/dbQueries.js
+++ Back-end/dbQueries.js
@@ -1,9 +1,8 @@
 // dbQueries.js
 
-
 const db = require("./modules/dbConfig.js");
 
-async function getProduct() {
+async function getAllProducts() {
   return new Promise((resolve, reject) => {
     db.query(
       "SELECT product_name, price, product_description" +
@@ -20,46 +19,72 @@ async function getProduct() {
   });
 }
 
-async function newProduct(product_name, product_category, product_color, product_photo, product_materials, price, sub_category, product_description, product_state) {
+async function newProduct(
+  product_name,
+  product_category,
+  product_color,
+  product_photo,
+  product_materials,
+  price,
+  sub_category,
+  product_description,
+  product_state
+) {
   return new Promise((resolve, reject) => {
     const sql =
       "INSERT INTO public.products (product_name, product_category, product_color, product_photo, product_materials, price, sub_category, product_description, product_state) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
-    const values = [product_name, product_category, product_color, product_photo, product_materials, price, sub_category, product_description, product_state];
+    const values = [
+      product_name,
+      product_category,
+      product_color,
+      product_photo,
+      product_materials,
+      price,
+      sub_category,
+      product_description,
+      product_state,
+    ];
 
-    db
-      .query(sql, values)
-      .then(results => {
+    db.query(sql, values)
+      .then((results) => {
         resolve(results.rows);
       })
-      .catch(error => {
+      .catch((error) => {
         reject(error);
       });
   });
 }
 
-async function searchProduct(argument) {
+async function getFilteredProducts(argument) {
   return new Promise((resolve, reject) => {
     const sql =
-    "SELECT product_name, product_category, product_color, product_photo, product_materials, price, sub_category, product_description, product_state" +
-    "FROM products"
-    "WHERE  " 
-    const values = [product_name, product_category, product_color, product_photo, product_materials, price, sub_category, product_description, product_state];
+      "SELECT product_name, product_category, product_color, product_photo, product_materials, price, sub_category, product_description, product_state" +
+      "FROM products";
+    ("WHERE  ");
+    const values = [
+      product_name,
+      product_category,
+      product_color,
+      product_photo,
+      product_materials,
+      price,
+      sub_category,
+      product_description,
+      product_state,
+    ];
 
-    db
-      .query(sql, values)
-      .then(results => {
+    db.query(sql, values)
+      .then((results) => {
         resolve(results.rows);
       })
-      .catch(error => {
+      .catch((error) => {
         reject(error);
       });
   });
 }
 
-
-
 module.exports = {
-  getProduct,
+  getAllProducts,
   newProduct,
-  searchProduct
+  getFilteredProducts,
 };
--- Back-end/routes/main.js
+++ Back-end/routes/main.js
@@ -2,21 +2,23 @@
 
 const express = require("express");
 const router = express.Router();
-const { getProduct } = require("./Back-end/dbQueries");
+const { getAllProducts } = require("./Back-end/dbQueries");
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   console.log("Router Working");
 });
-  
+
 router.get("/", async (req, res) => {
+  // QUOI : Query pour tous les produits disponible à la vente.
+  // QUAND : Dès l'affichage de la page d’accueil.
   try {
-    const products = await getProduct();
-    res.json(products);
+    const allProducts = await getAllProducts();
+    console.log("allProducts :", allProducts);
+    res.json(allProducts);
   } catch (error) {
-    console.error("Error executing SQL query", error);
+    console.error("Error executing the SQL query for all products", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
-
 
 module.exports = router;
