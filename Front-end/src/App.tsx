499058f2e6d1136009018be5940f5aff67b99f78
Tiki102 <146748603+Tiki102@users.noreply.github.com>
2024-03-08 11:12:53 +0100

    add files to frontend + changes in app file

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -3,6 +3,9 @@ import BackOffice from "./pages/BackOffice";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
+import Footer from './components/footer';
+import Sidebar from './components/sidedbar'; 
+
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -19,13 +22,15 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage title={"Accueil"} />} />
+          <Route path="/" element={<HomePage/>} />
           <Route path="/product" element={<Product />}/>
           <Route path="/login" element={<Login title={"Connection"} />}/>
           <Route path="/layout" element={<Layout title={"titre"}/>}/>
           {/* <Route path="/BackOffice" element={<BackOffice title={"titre"}/>}/> */}
           </Routes>
       </Router>
+      <Footer/>
+      <Sidebar/>
     </>
   );
 }
