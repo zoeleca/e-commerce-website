e58a029efe55a3dc0f6bfd346e825d08389d5e02
zoeleca <zoennts@gmail.com>
2024-03-11 10:30:14 +0100

    test gitignore

--- Back-end/modules/dbConfig.js
+++ Back-end/modules/dbConfig.js
@@ -3,7 +3,7 @@ const { Client } = require("pg");
 
 //Creation d'une instance du client PostgreSQL
 const client = new Client({
-  user: "morganelemoal",
+  user: "postgres",
   host: "localhost",
   database: "lovechaise",
   password: "postgres",
