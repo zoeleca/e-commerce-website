63b2b94ca53bba6b0ddd606d17a83726cfe375b5
zoeleca <zoennts@gmail.com>
2024-03-11 19:28:49 +0100

    sub_category added to 'allProducts' SQL query

--- Back-end/dbqueries/getProduct.js
+++ Back-end/dbqueries/getProduct.js
@@ -10,15 +10,19 @@ const allProducts = `SELECT
 p.id,
 p.product_name,
 pc.category_name,
+sc.sub_category_name,
 c.color_name,
 pp.photo_src,
 pm.material_name,
 p.price,
 p.product_description,
 ps.state_name
-FROM public.products p
+FROM
+public.products p
 INNER JOIN
 public.product_categories pc ON p.product_category = pc.id
+INNER JOIN
+public.sub_categories sc ON p.sub_category = sc.id
 INNER JOIN
 public.product_color c ON p.product_color = c.id
 INNER JOIN
