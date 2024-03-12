cd0e5c5eb5134b5484ca3036e8399435d4fab4a0
MorganeLM <lets.morganise.it@gmail.com>
2024-03-12 10:45:17 +0100

    update card

--- Front-end/src/components/Card.tsx
+++ Front-end/src/components/Card.tsx
@@ -1,30 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Product } from "../components/interface";
 
-// Étape 1 : Définir l'interface pour les propriétés de 'product'
-interface productInfo {
-  product_name: string;
-  product_description: string;
-  product_category: string;
-  sub_category: string;
-  product_color: string;
-  product_materials: string;
-  price: number;
-}
-
-// Étape 2 : Utiliser l'interface pour typer les props du composant
 interface CardProps {
-    productInfo: productInfo; 
+  productInfo: Product;
 }
 
-const Card: React.FC<CardProps> = ({ productInfo }) => { 
+const Card: React.FC<CardProps> = ({ productInfo }) => {
   return (
     <div style={{ border: "1px solid #ccc", padding: "20px", margin: "10px" }}>
       <h2>{productInfo.product_name}</h2>
       <p>Description: {productInfo.product_description}</p>
-      <p>Categorie: {productInfo.product_category}</p>
-      <p>Sous-categorie: {productInfo.sub_category}</p>
-      <p>Couleur: {productInfo.product_color}</p>
-      <p>Matière : {productInfo.product_materials}</p>
+      <p>Categorie: {productInfo.category_name}</p>
+      <p>Sous-categorie: {productInfo.sub_category_name}</p>
+      <p>Couleur: {productInfo.color_name}</p>
+      <p>Matière : {productInfo.material_name}</p>
       <p>Prix: {productInfo.price}€</p>
     </div>
   );
