6a4df4f324a82e43be8d3fb0225fe7eef789343b
MorganeLM <lets.morganise.it@gmail.com>
2024-03-12 15:27:51 +0100

    update app.tsx front

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,36 +1,30 @@
-//import Layout from './pages/Layout';
-//import Product from './pages/Product';
-
-import BackOffice from './pages/BackOffice';
+import BackOffice from "./pages/BackOffice";
 import HomePage from "./pages/HomePage";
-import Login from './pages/Login';
-import Post from './pages/Post';
-import Profile from './pages/Profile';
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import Profile from "./pages/Profile";
 
-import Footer from './components/footer';
-import Header from './components/Header';
-import ProductDetails from './components/ProductDetail';
-
-
+import Footer from "./components/footer";
+import Header from "./components/Header";
+import ProductDetails from "./components/ProductDetail";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/products" element={<ProductDetails />}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/post" element={<Post/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/backoffice" element={<BackOffice/>}/>
-
-          </Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/backoffice" element={<BackOffice />} />
+        </Routes>
       </Router>
-      <Footer/>
+      <Footer />
     </>
   );
 }
