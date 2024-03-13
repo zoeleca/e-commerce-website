863824a6c8280d0e6cfbace43bd66fe5b77253b3
MorganeLM <lets.morganise.it@gmail.com>
2024-03-13 12:35:02 +0100

    Update profile menu

--- Front-end/src/components/Header.tsx
+++ Front-end/src/components/Header.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import * as HoverCard from "@radix-ui/react-hover-card";
 import { PersonIcon } from "@radix-ui/react-icons";
 import { CardStackPlusIcon } from "@radix-ui/react-icons";
-import { HomeIcon } from "@radix-ui/react-icons";
+import { AvatarIcon } from "@radix-ui/react-icons";
 import { CalendarIcon } from "@radix-ui/react-icons";
-
 
 const Header = () => {
   console.log("This is a Header component");
@@ -12,25 +11,47 @@ const Header = () => {
     <HoverCard.Root openDelay={0}>
       <nav className="bg-transparent py-4 px-6 flex items-center justify-between shadow-md">
         <div>
-          <a href="/"><h1 className="text-3xl font-bold">L'Avenir du Passé</h1></a>
+          <a href="/">
+            <h1 className="text-3xl font-bold">L'Avenir du Passé</h1>
+          </a>
         </div>
         <div className="flex items-center space-x-4">
-          <HoverCard.Trigger asChild>
+          <a href="/Login">
             <PersonIcon className="w-12 h-12" />
+          </a>
+          <HoverCard.Trigger asChild>
+            <AvatarIcon className="w-12 h-12" />
           </HoverCard.Trigger>
           <HoverCard.Portal>
-            <HoverCard.Content className="HoverCardContent" sideOffset={5}>
+            <HoverCard.Content
+              className="bg-white p-4 w-64 h-auto"
+              sideOffset={5}
+            >
               <div>
-                <ul className="bg-white">
-                  <a href="/Profile"><li>Mon profil</li></a>
-                  <a href="/Cart"><li>Mon panier</li></a>
-                  <a href="/BackOffice"><li>Mon suivi</li></a>
+                <ul>
+                  <a href="/Profile">
+                    <li className="border-b border-gray-200 py-2">
+                      Mon profil
+                    </li>
+                  </a>
+                  <a href="/Cart">
+                    <li className="border-b border-gray-200 py-2">
+                      Mon panier
+                    </li>
+                  </a>
+                  <a href="/BackOffice">
+                    <li className="py-2">Mon suivi</li>
+                  </a>
                 </ul>
               </div>
             </HoverCard.Content>
           </HoverCard.Portal>
-          <a href="/Post"><CardStackPlusIcon className="w-12 h-12" /></a>
-          <a href="/Events"><CalendarIcon className="w-12 h-12" /></a>
+          <a href="/Post">
+            <CardStackPlusIcon className="w-12 h-12" />
+          </a>
+          <a href="/Events">
+            <CalendarIcon className="w-12 h-12" />
+          </a>
         </div>
       </nav>
     </HoverCard.Root>
